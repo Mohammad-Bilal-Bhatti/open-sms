ee472958222c7b507c300cb5e1121e02865bdd1b
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-21 16:01:43 +0500

    Innitial Commit: Initilize Repository.

---
+++ src/open/sms/OpenSms.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package open.sms;
+
+/**
+ *
+ * @author Bilal Bhatti
+ */
+public class OpenSms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

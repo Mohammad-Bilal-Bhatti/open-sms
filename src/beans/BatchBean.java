39d98e8559413de6b2a3a9c51eae793142c08644
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-22 17:33:47 +0500

    ADDED: New Batch Entity | Bean

---
+++ src/beans/BatchBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Javed Ali Dahri
+ */
+public class BatchBean extends Bean implements Serializable {
+    public BatchBean(){}
+    private Integer batchId;
+    private String batchName;
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Integer batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getBatchName() {
+        return batchName;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
+    
+}

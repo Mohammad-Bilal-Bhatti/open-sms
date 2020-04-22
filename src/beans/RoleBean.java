0634c8d4b69addc24cea1307467fbcb378e86125
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-22 14:11:48 +0500

    ADDED NEW: Role Entity | Bean

---
+++ src/beans/RoleBean.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Javed Ali Dahri
+ */
+public class RoleBean extends Bean implements Serializable{
+    
+    public RoleBean(){}
+    
+    private Integer roleId;
+    private String role;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

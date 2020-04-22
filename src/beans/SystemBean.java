c9ebd7af1a26016b8ed45802ce88e990040efdab
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-22 14:21:41 +0500

    ADDED NEW: System Entity | Bean MODIFIED: .gitignore build and dist

---
+++ src/beans/SystemBean.java
@@ -0,0 +1,47 @@
+package beans;
+
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Javed Ali Dahri
+ */
+public class SystemBean extends Bean implements Serializable{
+    
+    public SystemBean(){}
+    
+    private String macAddress;
+    private String date;
+    private String type;
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

0be398561097fd7e87163ae2f7afaf902f42ea20
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-23 12:52:06 +0500

    ADDED: New Shift Entity | Bean

---
+++ src/beans/ShiftBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Javed Ali Dahri
+ */
+public class ShiftBean extends Bean implements Serializable{
+    
+    public ShiftBean(){}
+    
+    private Integer shiftId;
+    private String shiftName;
+    private String startTime;
+    private String endTime;
+
+    public Integer getShiftId() {
+        return shiftId;
+    }
+
+    public void setShiftId(Integer shiftId) {
+        this.shiftId = shiftId;
+    }
+
+    public String getShiftName() {
+        return shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+    
+}

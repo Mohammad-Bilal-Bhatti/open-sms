598bf9467f1aeb812ac8985a0e7e586abb4792f3
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-26 00:28:12 +0500

    ADDED: New ImageHandler Class: responsible for storing and manipulating images

---
+++ src/utility/ImagesHandler.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributes;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.util.Date;
+
+/**
+ *
+ * @author Bilal Bhatti
+ */
+public class ImagesHandler {
+
+    /*
+    * Purpose: This class is responsible for storing and getting filess    
+     */
+
+    private final String imgDir;
+    private final int maxSize = 300; // in bytes
+
+    public ImagesHandler(String imgDir) {
+        this.imgDir = imgDir;
+        File dir = new File(imgDir);
+        if (!dir.exists()) {
+            dir.mkdir();
+            this.hide(dir.getAbsolutePath());
+        }
+    }
+
+    public boolean deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            return file.delete();
+        }
+        return false;
+    }
+
+    public ImageIcon loagImage(String path) {
+        ImageIcon imageIcon = new ImageIcon(path);
+        Image scaleImage = imageIcon.getImage().getScaledInstance(220, 180, Image.SCALE_DEFAULT);
+        return new ImageIcon(scaleImage);
+
+    }
+
+    /*
+    * @Purpose: save the image and return its path.
+     */
+    private String saveImage(String sourceFilePath) {
+        //First it validates the size of the file
+        String alteredImage = this.validateImageSize(sourceFilePath);
+        File sourceFile = new File(alteredImage);
+
+        String fileName = sourceFile.getName().toLowerCase();
+        String newFileName = this.generateFileName(fileName);
+
+        String destinationFilePath = this.imgDir + "\\" + newFileName;
+        File destinationFile = new File(destinationFilePath);
+
+        // Try to save the file.
+        try {
+            if (!destinationFile.exists()) {
+                destinationFile.createNewFile(); // Create file if not exists.
+            }
+            FileInputStream reader;
+            FileOutputStream writer;
+
+            byte[] readBytes = new byte[1024];
+
+            //Setting up the Reader and Speed Reader
+            reader = new FileInputStream(sourceFile);
+            //Setting up the Writer and Speed Writer
+            writer = new FileOutputStream(destinationFile);
+
+            int bytesRead;
+            while ((bytesRead = reader.read(readBytes)) != -1) {
+                writer.write(readBytes, 0, bytesRead); // Read and write on same time.
+                writer.flush();
+            }
+            reader.close();
+            writer.close();
+
+
+            this.hide(destinationFile.getAbsolutePath());
+            return destinationFile.getAbsolutePath();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public String generateFileName(String fileName) {
+        String currentDate = DateFormatter.formatDate(new Date()) + "-";
+        return currentDate + fileName;
+    }
+
+    public void hide(String path) {
+        Path p = Paths.get(path);
+        try {
+            DosFileAttributes dos = Files.readAttributes(p, DosFileAttributes.class);
+            Files.setAttribute(p, "dos:hidden", true);
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    public void unhide(String path) {
+        Path p = Paths.get(path);
+        try {
+            DosFileAttributes dos = Files.readAttributes(p, DosFileAttributes.class);
+            Files.setAttribute(p, "dos:hidden", false);
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    /*
+    * @Purpose: Validate to image size to certail thrashold value.
+     */
+    public String validateImageSize(String imagePath) {
+        File image = new File(imagePath);
+        long imageSizeInBytes = image.length();
+
+        //If the image size is greater than 300kb than resize the original image to 250x300px
+        if (imageSizeInBytes / 1024 > maxSize) {
+            return this.resizeImage(imagePath, 250, 300);
+        }
+        return imagePath;
+    }
+
+    /*
+    * @Purpose: Resize the image to specific width and height
+    * @Parms: image String: image path to convert
+    * @Parms: width int: width to convert
+    * @Parms: height int: height to convert
+    * @Return: String: path of converted image.
+     */
+    public String resizeImage(String image, int width, int height) {
+        try {
+            File originalFile = new File(image);
+            String parentPath = originalFile.getParent();
+            BufferedImage originalImage = ImageIO.read(originalFile);
+
+            int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB
+                    : originalImage.getType();
+
+            int IMG_WIDTH = width;
+            int IMG_CLAHEIGHT = height;
+
+            BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_CLAHEIGHT,
+                    type);
+
+            File replacedFile = new File(parentPath + "\\" + "Resized_" + originalFile.getName());
+            if (!replacedFile.exists()) {
+                replacedFile.createNewFile();
+            }
+            ImageIO.write(resizedImage, "png", replacedFile);
+
+            return replacedFile.getAbsolutePath();
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        return null;
+    }
+}

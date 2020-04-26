ab8ea9d8342b1ebf8038be7b4ac73d2e5d97b875
Mohammad Bilal Bhatti <dev.bilal.bhatti@gmail.com>
2020-04-26 20:16:22 +0500

    ADDED: New Batch and Course Dao Implementation.

---
+++ src/daoimpl/BatchDaoImpl.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daoimpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import beans.BatchBean;
+import connection.DBConnection;
+import dao.BatchDao;
+
+/**
+ *
+ * @author Bilal Bhatti
+ */
+public class BatchDaoImpl implements BatchDao{
+
+    @Override
+    public List<BatchBean> getAllBatches() {
+        List<BatchBean> batches = new ArrayList<>();
+        BatchBean batch = null;
+        try{
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from batch where active = ?");
+            pst.setString(1,"1");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                batch = new BatchBean();
+                batch.setBatchId(rs.getInt("batch_id"));
+                batch.setBatchName(rs.getString("batch_title"));
+                batches.add(batch);
+            }
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        
+        return batches;
+    }
+
+    @Override
+    public int addBatch(BatchBean batch) {
+        try{
+             PreparedStatement pst = DBConnection.getConnection().prepareStatement("Insert into batch(batch_title,active,created_by,created_date) values(?,?,?,?)");
+             pst.setString(1, batch.getBatchName());
+             pst.setString(2, "1");
+             pst.setInt(3, batch.getCreatedBy());
+             pst.setString(4, batch.getCreatedDate());
+             return pst.executeUpdate();
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        
+        return 0;
+    }
+
+    @Override
+    public int updateBatch(BatchBean batch) {
+        try{
+               PreparedStatement pst = DBConnection.getConnection().prepareStatement("Update batch set batch_title=?,modified_by = ?,modified_date = ? where batch_id = ?");
+               pst.setString(1, batch.getBatchName());
+               pst.setInt(2, batch.getModifiedBy());
+               pst.setString(3, batch.getModifiedDate());
+               pst.setInt(4, batch.getBatchId());
+               return pst.executeUpdate();
+        }
+        catch(SQLException ex){}
+        
+        return 0;
+    }
+
+    @Override
+    public int deleteBatch(BatchBean batch) {
+        try{
+             PreparedStatement pst = DBConnection.getConnection().prepareStatement("Update batch set active = ?,modified_by = ?,modified_date = ? where batch_id = ?");
+             pst.setString(1, "0");
+             pst.setInt(2, batch.getModifiedBy());
+             pst.setString(3, batch.getModifiedDate());
+             pst.setInt(4, batch.getBatchId());
+             return pst.executeUpdate();
+        }
+        catch(SQLException ex){}
+                
+        return 0;
+    }
+
+    @Override
+    public BatchBean getBatchById(Integer batchId) {
+        BatchBean batch  = null;
+        try{
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from batch where active = ? and batch_id = ?");
+            pst.setString(1, "1");
+            pst.setInt(2, batchId);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                batch = new BatchBean();
+                batch.setBatchId(batchId);
+                batch.setBatchName(rs.getString("batch_title"));
+            }
+        }
+        catch(SQLException ex){}
+        
+        return batch;
+    }
+    
+}
---
+++ src/daoimpl/CourseDaoImpl.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daoimpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import beans.CourseBean;
+import connection.DBConnection;
+import dao.CourseDao;
+
+/**
+ *
+ * @author Bilal Bhatti
+ */
+public class CourseDaoImpl implements CourseDao {
+
+    @Override
+    public List<CourseBean> getAllCourses() {
+        List<CourseBean> courses = new ArrayList<>();
+        CourseBean course = null, mainCourse = null;
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from course where active = ?");
+            pst.setString(1, "1");
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                course = new CourseBean();
+                if (rs.getString("main_course") != null) {
+                    mainCourse = getParentCourse(rs.getInt("main_course"));
+                } else {
+                    mainCourse = new CourseBean();
+                    mainCourse.setCourseName(null);
+                }
+                course.setCourseId(rs.getInt("course_id"));
+                course.setCourseName(rs.getString("course_title"));
+                course.setCourseDuration(rs.getString("duration"));
+                course.setMainCourse(mainCourse);
+                courses.add(course);
+            }
+        } catch (SQLException ex) {
+        }
+
+        return courses;
+    }
+
+    @Override
+    public int addCourse(CourseBean course) {
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Insert into course(course_title,duration,main_course,active,created_by,created_date) values(?,?,?,?,?,?)");
+            pst.setString(1, course.getCourseName());
+            pst.setString(2, course.getCourseDuration());
+            if(course.getMainCourse().getCourseId()==0){
+                pst.setString(3,null);
+            }
+            else{
+                pst.setInt(3, course.getMainCourse().getCourseId());
+            }
+            pst.setString(4,"1");
+            pst.setInt(5, course.getCreatedBy());
+            pst.setString(6, course.getCreatedDate());
+            return pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int updateCourse(CourseBean course) {
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Update course set course_title = ?,duration = ?,main_course = ?,modified_by = ?,modified_date = ? where course_id=?");
+            pst.setString(1, course.getCourseName());
+            pst.setString(2, course.getCourseDuration());
+            if(course.getMainCourse().getCourseId()==0){
+                pst.setString(3,null);
+            }
+            else{
+                pst.setInt(3, course.getMainCourse().getCourseId());
+            }
+            pst.setInt(4, course.getModifiedBy());
+            pst.setString(5, course.getModifiedDate());
+            pst.setInt(6,course.getCourseId());
+            return pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int deleteCourse(CourseBean course) {
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Update course set active = ?,modified_by = ?,modified_date=? where course_id=? or main_course = ?");
+            pst.setString(1, "0");
+            pst.setInt(2, course.getModifiedBy());
+            pst.setString(3, course.getModifiedDate());
+            pst.setInt(4, course.getCourseId());
+            pst.setInt(5, course.getCourseId());
+            return pst.executeUpdate();
+        } catch (SQLException ex) {
+        }
+
+        return 0;
+    }
+
+    @Override
+    public CourseBean getCourseById(Integer courseId) {
+        CourseBean course = null, main_course = null;
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from course where active = ? and course_id = ?");
+            pst.setString(1, "1");
+            pst.setInt(2, courseId);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                course = new CourseBean();
+                main_course = new CourseBean();
+                main_course.setCourseId(rs.getInt("main_course"));
+                course.setCourseId(courseId);
+                course.setCourseName(rs.getString("course_title"));
+                course.setCourseDuration(rs.getString("duration"));
+                course.setMainCourse(main_course);
+            }
+        } catch (SQLException ex) {
+        }
+
+        return course;
+    }
+
+    public CourseBean getParentCourse(Integer id) {
+        CourseBean mainCourse = new CourseBean();
+        try {
+            PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from course where course_id = ?");
+            pst.setInt(1, id);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                mainCourse.setCourseId(id);
+                mainCourse.setCourseName(rs.getString("course_title"));
+                mainCourse.setCourseDuration(rs.getString("duration"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return mainCourse;
+    }
+
+    @Override
+    public List<CourseBean> getAllParentCourses() {
+        List<CourseBean>parentCourses = new ArrayList<>();
+        CourseBean parentCourse  =null;
+        try{
+                PreparedStatement pst = DBConnection.getConnection().prepareStatement("Select * from course where main_course is null and active = ?");
+                pst.setString(1, "1");
+                ResultSet rs = pst.executeQuery();
+                while(rs.next()){
+                    parentCourse  = new CourseBean();
+                    parentCourse.setCourseId(rs.getInt("course_id"));
+                    parentCourse.setCourseName(rs.getString("course_title"));
+                    parentCourse.setCourseDuration(rs.getString("duration"));
+                    parentCourses.add(parentCourse);
+                }
+        }
+        catch(SQLException ex){
+        }
+        return parentCourses;
+    }
+    
+    
+}
